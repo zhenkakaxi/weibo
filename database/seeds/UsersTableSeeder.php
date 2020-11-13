5042728cf258c2b06438eb4041e301d47976e3b2
2284624967 <2284624967@qq.com>
2020-11-13 08:42:26 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -7,11 +7,12 @@ class UsersTableSeeder extends Seeder
 {
     public function run()
     {
-        factory(User::class)->times(50)->create();
+        $users = factory(User::class)->times(50)->create();
 
         $user = User::find(1);
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
